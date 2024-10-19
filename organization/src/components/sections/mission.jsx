de3dfef324a3f1f3aef1cdfd41b72614d07d1196
Rana <ariyanmahmud02019@gmail.com>
2024-10-20 01:16:16 +0600

    mission vission

--- organization/src/components/sections/mission.jsx
+++ organization/src/components/sections/mission.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { missionVission } from '../../assets/data/fakedata'
 
 export default function Mission_Vission() {
   return (
-    <div>
-      
+    <div className='mission-section'>
+        <div className="flex justify-between">
+            <div className="image-site">
+                <span>
+                    <img src={''} alt={missionVission.image.alt} />
+                </span>
+            </div>
+            <div className="text-site">
+                <div className="mision">
+                  <span>
+                      <h3>{missionVission.mission.title}</h3>
+                  </span>
+                  <p>{missionVission.mission.text}</p>
+                </div>
+                <div className="vission">
+                  <span>
+                      <h3>{missionVission.vission.title}</h3>
+                  </span>
+                  <p>{missionVission.vission.text}</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
